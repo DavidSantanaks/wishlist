33c0ff111ac5b2428c90d9b99787cb3ce973b5c2
DavidDSantanaks <davidfjobs@gmail.com>
2023-10-14 21:24:16 -0300

    ADD comments in wishListController

--- src/main/java/br/com/david/Wishlist/wishlist/WishlistController.java
+++ src/main/java/br/com/david/Wishlist/wishlist/WishlistController.java
@@ -15,13 +15,14 @@ public class WishlistController {
     @Autowired
     WishlistService wishlistService;
 
-
+    //Busca todos os desejos
     @GetMapping("/")
     public ResponseEntity<List<WishlistModel>> findAllWish(){
         List<WishlistModel> allWish = this.wishlistService.findAllWish();
         return ResponseEntity.ok().body(allWish);
     }
 
+    //Busca todos os desejos
     @GetMapping("/{id}")
     public ResponseEntity<WishlistModel> findOneWish(@PathVariable  UUID id){
         WishlistModel oneWish = this.wishlistService.findById(id);
@@ -30,16 +31,17 @@ public class WishlistController {
         }
         return ResponseEntity.ok().body(oneWish);
     }
-
+    //Cadastra um desejo
     @PostMapping("/")
     public ResponseEntity createWish(@RequestBody WishlistModel wishlistModel){
         WishlistModel newWish = this.wishlistService.createWish(wishlistModel);
         return ResponseEntity.ok().body(wishlistModel);
     }
 
+    //Deleta um desejo
     @DeleteMapping("/{id}")
     public ResponseEntity deleteWish(@PathVariable  UUID id){
-
+        //Arrumar essa classe
         if(this.wishlistService.findById(id) != null){
             this.wishlistService.deleteWish(id);
             return ResponseEntity.ok().body("Wish deleted :/");
